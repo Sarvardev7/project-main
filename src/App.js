763823bfc8e42a62fe275629a21b999f8537cbc9
[Sarvar qahhorov] <[qahhorov2012@gmail.com]>
2024-11-01 18:44:40 +0300

    project qoshildi

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import First from "./components/first"
+import React from 'react';
+
+
+const App = () => {
+  return (
+    <div>
+      <First/>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/first.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const First = () => {
+    return (
+        <div>
+         <div className="flex items-beetwen justify-center">
+         <div className="flex ">
+                <p className='font-bold text-black'>Furniture</p>
+                <p className='font-bold text-black'>Why Feather</p>
+                <p className='font-bold text-black'>Feather for Business</p>
+            </div>
+            <div className="">
+                <h1 className='font-bold text-black'>Feather</h1>
+            </div>
+            <div className="">
+                <p className='font-bold text-black'>Check if We Deliver</p>
+                <p className='font-bold text-black'>Search</p>
+                <p className='font-bold text-black'> Account</p>
+            </div>
+         </div>
+        </div>
+    );
+}
+
+export default First;
